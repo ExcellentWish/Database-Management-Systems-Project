c5ad965fabee252bff219dfe2191dd63c2d90924
Nigel Doheny <nigel.doheny93@gmail.com>
2022-08-25 10:20:25 +0000

    Add task html

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,8 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     //sidenave initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-
-     let modal = document.querySelectorAll('.modal');
-     M.Modal.init(modal);
-  });
+    // modal
+    let modal = document.querySelectorAll('.modal');
+    M.Modal.init(modal);
+    // datepicker
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+    // select in add task
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+});
