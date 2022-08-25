21240ccb5be6580ff2fe981f13118edee03542be
Nigel Doheny <nigel.doheny93@gmail.com>
2022-08-25 09:37:43 +0000

    Add delete and modal for delete_category

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     //sidenave initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+     let modal = document.querySelectorAll('.modal');
+     M.Modal.init(modal);
   });
