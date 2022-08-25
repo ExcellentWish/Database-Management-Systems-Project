50ba5f0ed523974639e41317b39ca634aab58730
Nigel Doheny <nigel.doheny93@gmail.com>
2022-08-25 13:43:24 +0000

    fix __init__py if else

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -12,8 +12,9 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    
 else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
     uri = os.environ.get("DATABASE_URL")    
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://",1)
